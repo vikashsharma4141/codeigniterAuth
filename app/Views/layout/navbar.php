3d467d9c02d512cd29c196b8a4d63bacad661e5c
Vikash Sharma <138751024+vikashsharma4141@users.noreply.github.com>
2025-03-21 12:35:51 +0530

    Create navbar.php

---
+++ app/Views/layout/navbar.php
@@ -0,0 +1,9 @@
+<nav class="navbar">
+    <div class="container">
+        <a href="<?= base_url('/'); ?>" class="logo">MyApp</a>
+        <ul>
+            <li><a href="<?= base_url('profile'); ?>">Profile</a></li>
+            <li><a href="<?= base_url('logout'); ?>">Logout</a></li>
+        </ul>
+    </div>
+</nav>
