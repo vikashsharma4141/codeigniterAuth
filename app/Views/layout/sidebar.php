58999b5781a1a7cbb07874f3a40cff20ef78e38c
Vikash Sharma <138751024+vikashsharma4141@users.noreply.github.com>
2025-03-21 12:35:07 +0530

    Create sidebar.php

---
+++ app/Views/layout/sidebar.php
@@ -0,0 +1,7 @@
+<div class="sidebar">
+    <ul>
+        <li><a href="<?= base_url('admin/dashboard'); ?>">Admin Dashboard</a></li>
+        <li><a href="<?= base_url('user/dashboard'); ?>">User Dashboard</a></li>
+        <li><a href="<?= base_url('logout'); ?>">Logout</a></li>
+    </ul>
+</div>
